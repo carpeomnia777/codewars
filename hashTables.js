d89a8e6606ef45044b6b1e8bd03c403ff4d47632
Chris W <41281925+carpeomnia777@users.noreply.github.com>
2022-01-24 04:17:58 -0500

    write a function that contain object of rooms

---
+++ hashTables.js
@@ -0,0 +1,17 @@
+var rooms = {
+    horror:{
+      name:'slaughterhouse',
+      description:'scary',
+      completed:'false'
+    },
+    steampunk:{
+      name:'factory',
+      description:'moist',
+      completed:'true'
+    },
+    restaurant:{
+      name:'bakery',
+      description:'smells great',
+      completed:'true'
+    }
+  }
